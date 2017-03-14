86e033f14f8821e22ce4f32c205b9d92b4de109a
Akira Youngblood <akirayoungblood@me.com>
2017-03-14 17:29:28 -0600

    Most of the requester thread stuff done

--- pa3/assignment/multi-lookup.c
+++ pa3/assignment/multi-lookup.c
@@ -7,25 +7,50 @@
 
 #include "multi-lookup.h"
 
+// Global queue
 const int queueSize = 20;
+queue q;
+// Global output file
+FILE* outputfp = NULL;
+// Global locks
+pthread_mutex_t queue_lock, output_lock;
 
 int main(int argc, char *argv[]) {
     int i, rv;
     // Parse command-line arguments
-    if (argc <= 1) {
-        // No input files, warn and print usage
-        fprintf(stderr,"No input files specified.\n");
+    if (argc <= 2) {
+        // Need at least two file names (in and out), warn and print usage
+        fprintf(stderr,"Not enough arguments provided.\n");
         fprintf(stderr,"Usage:\n"
-                       "  resolve file [...]\n");
-        return 1; // return an error
+                       "  resolve infile [infile2 ...] outfile\n");
+        return EXIT_FAILURE;
     }
-    // We have at least one input file
-    for (i = 1; i < argc; ++i) {
-        printf("%s\n",argv[i]);
+    // We have at least one input file and an output file
+    /* DEBUG */
+    for (i = 1; i < argc-1; ++i) {
+        printf("Input file: %s\n",argv[i]);
+    }
+    printf("Output file: %s\n",argv[argc-1]);
+    /* END DEBUG */
+    // Open the output file
+    outputfp = fopen(argv[argc-1],"w");
+    if (!outputfp) {
+        fprintf(stderr,"Unable to open output file, exiting.\n");
+        return EXIT_FAILURE;
+    }
+    // Initialize the queue
+    if (queue_init(&q,queueSize) == QUEUE_FAILURE){
+        fprintf(stderr,"Error: queue_init failed!\n");
+        return EXIT_FAILURE;
+    }
+    // Initialize the locks
+    if (pthread_mutex_init(&queue_lock,NULL) || pthread_mutex_init(&output_lock,NULL)) {
+        fprintf(stderr,"Error: pthread_mutex_init failed!\n");
+        return EXIT_FAILURE;
     }
     // Create a requester thread pool based on number of input files
     // Some may be invalid, but that is handled by the threads
-    const int NUM_THREADS_RQR = argc-1;
+    const int NUM_THREADS_RQR = argc-2;
     pthread_t threads_rqr[NUM_THREADS_RQR];
     for (i = 0; i < NUM_THREADS_RQR; ++i) {
         fprintf(stderr,"Spawning requester thread %d\n", i);
@@ -40,80 +65,51 @@ int main(int argc, char *argv[]) {
     for (i = 0; i < NUM_THREADS_RQR; i++){
            pthread_join(threads_rqr[i],NULL);
     }
-    printf("All requester threads have finished\n");
+    printf("All requester threads have finished, printing queue contents:\n");
 
-
-    // Create a queue
-    queue q;
-    if (queue_init(&q, queueSize) == QUEUE_FAILURE){
-        fprintf(stderr, "error: queue_init failed!\n");
-    }
-    /*
-    // Test for empty queue when empty
-    if (!queue_is_empty(&q)) {
-        fprintf(stderr,"error: queue should report empty\n");
-    }
-    // Test for full queue when empty
-    if (queue_is_full(&q)) {
-        fprintf(stderr,"error: queue should report empty\n");
-    }
-    */
-    return 0;
-
-    /*
-    // Setup Local Vars
-    pthread_t threads[NUM_THREADS];
-    int rc;
-    long t;
-    long cpyt[NUM_THREADS];
-
-    // Spawn NUM_THREADS threads
-    for(t=0;t<NUM_THREADS;t++){
-        printf("In main: creating thread %ld\n", t);
-        cpyt[t] = t;
-        rc = pthread_create(&(threads[t]), NULL, PrintHello, &(cpyt[t]));
-        if (rc){
-            printf("ERROR; return code from pthread_create() is %d\n", rc);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    // Wait for All Theads to Finish
-    for(t=0;t<NUM_THREADS;t++){
-           pthread_join(threads[t],NULL);
-    }
-    printf("All of the threads were completed!\n");
-
-    */
-    /* Last thing that main() should do */
-    /* pthread_exit unnecessary due to previous join */
-    //pthread_exit(NULL);
-
+    fclose(outputfp);
+    pthread_mutex_destroy(&queue_lock);
+    pthread_mutex_destroy(&output_lock);
     return 0;
 }
 
+// Run by each requester thread.
+// Opens file, adds hostnames to queue, and exits
 void* RequesterThreadAction(void* file_name) {
     fprintf(stderr,"RequesterThreadAction: %s\n",(char *)file_name);
-    return NULL;
-}
-
-/* Function for Each Thread to Run */
-void* PrintHello(void* threadid) {
-    /* Setup Local Vars and Handle void* */
-    long* tid = threadid;
-    long t;
-    long numprint = 3;
-
-    /* Print hello numprint times */
-    for(t=0; t<numprint; t++)
-    {
-        printf("Hello World! It's me, thread #%ld! "
-           "This is printout %ld of %ld\n",
-           *tid, (t+1), numprint);
-        /* Sleep for 1 to 2 Seconds */
-        usleep((rand()%10)*100+1000);
+    // Try to open the file
+    FILE* fp = fopen((char*)file_name,"r");
+    if (!fp) {
+        fprintf(stderr,"Failed to open input file %s\n",(char*)file_name);
+        return NULL; // exit quietly
     }
-
-    /* Exit, Returning NULL*/
+    // File opened succesfully, read lines and add to queue
+    char hostname[1025];
+    while (fscanf(fp, "%1024s", hostname) > 0) {
+        pthread_mutex_lock(&queue_lock);
+        // Spin on full queue, sleeping for random time between 0-100 us
+        while (queue_is_full(&q)) {
+            pthread_mutex_unlock(&queue_lock);
+            usleep(rand()%100);
+            pthread_mutex_lock(&queue_lock);
+        }
+        // Put the hostname on the heap so we can queue it.
+        // malloc'ed memory is freed by resolver threads
+        char* temp = (char*)malloc(strlen(hostname)+1);
+        if (temp == NULL) {
+            fprintf(stderr,"Out of memory. Thread halting.\n");
+            return NULL; // exit quietly
+        }
+        strcpy(temp, hostname);
+        // Add to queue and stop if something goes horribly wrong
+        if (queue_push(&q, temp) == QUEUE_FAILURE) {
+            fprintf(stderr,"Failed to push to queue. Thread halting.\n");
+            return NULL; // exit quietly
+        }
+        // printf("%s: %s\n",(char*)file_name,hostname);
+        pthread_mutex_unlock(&queue_lock);
+    }
+    // Processed all lines in the file, close the file and halt
+    fclose(fp);
     return NULL;
 }
